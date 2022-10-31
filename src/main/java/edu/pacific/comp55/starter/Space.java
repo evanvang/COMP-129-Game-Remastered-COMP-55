75917c075a7d82bab8cc2f7ec748bf93349bbb47
Evan Vang <vangevan62@gmail.com>
2022-10-31 16:36:56 -0700

    Added Space class

--- src/main/java/edu/pacific/comp55/starter/Space.java
+++ src/main/java/edu/pacific/comp55/starter/Space.java
@@ -1,14 +1,42 @@
 package edu.pacific.comp55.starter;
 
 public class Space {
-
-	public Space() {
-		// TODO Auto-generated constructor stub
+	
+	public String toString() {
+		return "r" + this.row + "c" + this.col;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private int row;
+	private int col;
 
+	public Space(int row, int col) {
+		this.row = row;
+		this.col = col;
 	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+//	public Space() {
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
 
 }
