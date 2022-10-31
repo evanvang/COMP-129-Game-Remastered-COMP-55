441a2960b42385032b35cf69ec7c329f01b443e7
anhth <anhth@10.15.143.44>
2022-10-31 16:28:04 -0700

    HN: just created the enum

--- src/main/java/edu/pacific/comp55/starter/characterType.java
+++ src/main/java/edu/pacific/comp55/starter/characterType.java
@@ -1,5 +1,14 @@
 package edu.pacific.comp55.starter;
 
 public enum characterType {
-
+PLAYER, ENEMY, BOSS;
+	
+	public String toString() {
+		switch(this) {
+			case PLAYER: return "player";
+			case ENEMY: return "enemy";
+			case BOSS: return "boss";
+		}
+		return "n/a";
+	}
 }
