d9a9d59e41d17f66ad79e3119425a660516390dc
anhth <anhth@10.15.143.44>
2022-11-02 16:18:33 -0700

    HN: somepane in

--- src/main/java/edu/pacific/comp55/starter/SomePane.java
+++ src/main/java/edu/pacific/comp55/starter/SomePane.java
@@ -1,5 +1,5 @@
 package edu.pacific.comp55.starter;
-import java.awt.event.MouseEvent;
+/*import java.awt.event.MouseEvent;
 
 import acm.graphics.GImage;
 import acm.graphics.GObject;
@@ -36,5 +36,87 @@ public class SomePane extends GraphicsPane {
 		if (obj == img) {
 			program.switchToMenu();
 		}
+	}
+}*/
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GImage;
+import acm.graphics.GObject;
+
+public class SomePane extends GraphicsPane {
+	private MainApplication program; // you will use program to get access to
+									// all of the GraphicsProgram calls
+	private GImage img;
+	private GParagraph para;
+	private Player player;
+
+	public SomePane(MainApplication app) {
+		this.program = app;
+		img = new GImage("robot head.jpg", 100, 100);
+		para = new GParagraph("welcome\nto my\nsecret room!", 150, 300);
+		para.setFont("Arial-24");
+		
+	}
+
+	@Override
+	public void showContents() {
+		program.add(img);
+		program.add(para);
+	}
+
+	@Override
+	public void hideContents() {
+		program.remove(img);
+		program.remove(para);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		para.setText("you need\nto click\non the eyes\nto go back");
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		if (obj == img) {
+			program.switchToMenu();
+		}
+	}
+	
+
+	
+	public void keyPressed(int k) {
+		if (k == KeyEvent.VK_A)
+			player.setLeft(true);
+		if (k == KeyEvent.VK_D)
+			player.setRight(true);
+		if (k == KeyEvent.VK_S)
+			player.setDown(true);
+		if (k == KeyEvent.VK_W)
+			player.startJumping(true);
+		if (k == KeyEvent.VK_UP)
+			player.startJumping(true);
+		if (k == KeyEvent.VK_RIGHT)
+			player.setRight(true);
+		if (k == KeyEvent.VK_DOWN)
+			player.setDown(true);
+		if (k == KeyEvent.VK_LEFT)
+			player.setLeft(true);
+	}
+
+	public void keyReleased(int k) {
+		if (k == KeyEvent.VK_A)
+			player.setLeft(false);
+		if (k == KeyEvent.VK_D)
+			player.setRight(false);
+		if (k == KeyEvent.VK_S);
+			player.setDown(false);
+		if (k == KeyEvent.VK_W)
+			player.startJumping(false);
+		if (k == KeyEvent.VK_UP)
+			player.startJumping(false);
+		if (k == KeyEvent.VK_RIGHT)
+			player.setRight(false);
+		if (k == KeyEvent.VK_DOWN)
+			player.setDown(false);
+		if (k == KeyEvent.VK_LEFT)
+			player.setLeft(false);
 	}
 }
