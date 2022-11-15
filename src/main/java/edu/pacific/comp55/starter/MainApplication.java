175880c9a3ba9cf4587bbc8e8cff2406093c4297
Thu Nguyen <p_nguyen64@u.pacific.edu>
2022-11-14 16:30:36 -0800

    Refactor names for main menue

--- src/main/java/edu/pacific/comp55/starter/MainApplication.java
+++ src/main/java/edu/pacific/comp55/starter/MainApplication.java
@@ -23,19 +23,20 @@ public class MainApplication extends GraphicsApplication {
 
     public void run() {
 	System.out.println("Hello, world!");
-	mainGame = new MainGame(this);
+	//mainGame = new MainGame(this);
 	menu = new MenuPane(this);
-	settings = new SettingsPane(this);
+	//settings = new SettingsPane(this);
 	setupInteractions();
 	switchToMenu();
     }
 
+    //Start Screen
     public void switchToMenu() {
 	count++;
 	switchToScreen(menu);
     }
 
-    public void switchToSome() {
+    public void switchToLevel() {
 	switchToScreen(mainGame);
     }
 
--- src/main/java/edu/pacific/comp55/starter/MenuPane.java
+++ src/main/java/edu/pacific/comp55/starter/MenuPane.java
@@ -10,42 +10,46 @@ import acm.graphics.GObject;
 public class MenuPane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 	// all of the GraphicsProgram calls
-	private GButton rect;
-	private GButton settings;
-	private GImage menu;
+	private GButton playButton;
+	private GButton settingsButton;
+	private GImage menuBackground;
 	
 	public MenuPane(MainApplication app) {
 		super();
 		program = app;
-		rect = new GButton("Play", 300, 450, 150, 75);
-		rect.setFillColor(Color.WHITE);
-		menu = new GImage("Intro.png", -12, 0);
-		settings = new GButton("Settings", 600, 450, 150, 75);
-		settings.setFillColor(Color.WHITE);
+		
+		playButton = new GButton("Play", 300, 450, 150, 75);
+		playButton.setFillColor(Color.WHITE);
+		
+		settingsButton = new GButton("Settings", 600, 450, 150, 75);
+		settingsButton.setFillColor(Color.WHITE);
+		
+		menuBackground = new GImage("Intro.png", -12, 0);
+
 	}
 
 	@Override
 	public void showContents() {
-		program.add(menu);
-		program.add(rect);
-		program.add(settings);
+		program.add(menuBackground);
+		program.add(playButton);
+		program.add(settingsButton);
 	}
 
 	@Override
 	public void hideContents() {
-		program.remove(menu);
-		program.remove(rect);
-		program.remove(settings);
+		program.remove(menuBackground);
+		program.remove(playButton);
+		program.remove(settingsButton);
 		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == rect) {
-			program.switchToSome();
+		if (obj == playButton) {
+			program.switchToLevel();
 		}
-		if (obj == settings) {
+		if (obj == settingsButton) {
 			program.switchToSettings();
 		}
 	}
