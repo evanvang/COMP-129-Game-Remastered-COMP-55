421ba0a427943bced8eaf2bd495062cf83abaa95
Thu Nguyen <p_nguyen64@u.pacific.edu>
2022-11-14 17:07:34 -0800

    changed window size

--- src/main/java/edu/pacific/comp55/starter/MainApplication.java
+++ src/main/java/edu/pacific/comp55/starter/MainApplication.java
@@ -7,8 +7,8 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class MainApplication extends GraphicsApplication {
-    public static final int WINDOW_WIDTH = 800;
-    public static final int WINDOW_HEIGHT = 600;
+    public static final int WINDOW_WIDTH = 1900;
+    public static final int WINDOW_HEIGHT = 850;
     public static final String MUSIC_FOLDER = "sounds";
     private static final String[] SOUND_FILES = { "r2d2.mp3", "somethinlikethis.mp3" };
 
