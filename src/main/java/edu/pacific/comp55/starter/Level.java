a0ea9a90eddf123fc813e956b916c3b7a5ced09e
Thu Nguyen <p_nguyen64@u.pacific.edu>
2022-11-16 02:33:09 -0800

    timer->swingtimer test

--- src/main/java/edu/pacific/comp55/starter/Level.java
+++ src/main/java/edu/pacific/comp55/starter/Level.java
@@ -25,11 +25,11 @@ public class Level implements ActionListener{
     private Map map;
     private Player player;
     private Enemy enemy;
-    private Timer timer;
+    private javax.swing.Timer  timer;
 
     // Constructor
     public Level(MainApplication program, int levelNum) {
-    	this.timer = new Timer(500, this);
+    	this.timer = new javax.swing.Timer(100, this);
     	mainScreen = program;
     	map = new Map();
     	startTimer();
