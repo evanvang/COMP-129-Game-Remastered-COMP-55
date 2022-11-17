8d186f9095172cfff698288871224ed584c47b7b
Thu Nguyen <p_nguyen64@u.pacific.edu>
2022-11-16 19:53:57 -0800

    map modify

--- src/main/java/edu/pacific/comp55/starter/Map.java
+++ src/main/java/edu/pacific/comp55/starter/Map.java
@@ -14,9 +14,16 @@ public class Map {
 
     // Array list holds GRect objects for our game world
     private ArrayList<Chunk> chunks;
+    private ArrayList<Enemy> enemies;
 
     public ArrayList<Chunk> getChunks() {
 		return chunks;
+	}
+    
+    
+
+    public ArrayList<Enemy> getEnemies() {
+		return enemies;
 	}
 
 	public void setChunks(ArrayList<Chunk> chunks) {
@@ -26,6 +33,7 @@ public class Map {
 	// Constructor
     public Map() {
 	this.chunks = new ArrayList<>();
+	this.enemies = new ArrayList<Enemy>();
     }
 
     // Method to create a chunk, adds to the ArrayList "chunkToGRect"
@@ -33,6 +41,11 @@ public class Map {
 	Chunk chunk = new Chunk(chunkName, path, chunkX, chunkY, width, height);
 
 	chunks.add(chunk);
+    }
+    
+    public void createEnemy ( int startX, int startY) {
+    	Enemy enemy = new Enemy(startX, startY);
+    	enemies.add(enemy);
     }
 
 }
