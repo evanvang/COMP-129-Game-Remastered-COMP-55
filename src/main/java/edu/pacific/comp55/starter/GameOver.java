18df163389ea3b249177a3fde043c31ab1b9fe46
Ethan Donley <fruitahloops@gmail.com>
2022-12-08 18:59:02 -0800

    Added Win Screen (not implemented yet), Game Over/Win Screen Text

--- src/main/java/edu/pacific/comp55/starter/GameOver.java
+++ src/main/java/edu/pacific/comp55/starter/GameOver.java
@@ -1,5 +1,7 @@
 package edu.pacific.comp55.starter;
 import java.awt.event.MouseEvent;
+
+import acm.graphics.GImage;
 import acm.graphics.GObject;
 
 public class GameOver extends GraphicsPane {
@@ -7,24 +9,28 @@ public class GameOver extends GraphicsPane {
 	private MainApplication program;
 	private GButton retry2;
 	private GButton quit2;
+	private GImage gOver;
 	private Level level2;
 	private int numLevel;
 	
 	public GameOver(MainApplication app, Level l) {
 		program = app;
 		level2 = l;
+		gOver = new GImage("Game Over.png", 500, 150);
 		retry2 = new GButton("Retry", 580, 375, 150, 75);
-		quit2 = new GButton("Quit", 580, 450, 150, 75);
+		quit2 = new GButton("Quit", 580, 500, 150, 75);
 	}
 	
 	@Override
 	public void showContents() {
+		program.add(gOver);
 		program.add(quit2);
 		program.add(retry2);
 	}
 	
 	@Override
 	public void hideContents() {
+		program.remove(gOver);
 		program.remove(quit2);
 		program.remove(retry2);
 		level2.setGameOverToNull();
---
+++ src/main/java/edu/pacific/comp55/starter/WinScreen.java
@@ -0,0 +1,42 @@
+package edu.pacific.comp55.starter;
+
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GImage;
+import acm.graphics.GObject;
+
+public class WinScreen extends GraphicsPane {
+
+	private MainApplication program;
+	private GButton goBack;
+	private GImage winText;
+	
+	public WinScreen(MainApplication app) {
+		program = app;
+		goBack = new GButton("Go Back", 580, 375, 150, 75);
+		winText = new GImage("You Win!.png", 100, 100);
+
+	}
+	
+	@Override
+	public void showContents() {
+		program.add(goBack);
+		program.add(winText);
+	}
+	
+	@Override
+	public void hideContents() {
+		program.remove(goBack);
+		program.add(winText);
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		if (obj == goBack) {
+			 program.removeAll();
+	         program.switchToMenu();
+	  
+		}
+	}
+}
